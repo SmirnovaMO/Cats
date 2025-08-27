e66bfd2cacd9fea06f275160e355664f62e9496d
892639o8773-lgtm <892639o8773@gmail.com>
2025-08-27 19:31:00 +0300

    Добавлено получение изображений по тегу, а также без тега.

--- Cataas.py
+++ Cataas.py
@@ -19,7 +19,9 @@ def load_image(url):
 
 
 def open_new_window():
-    img = load_image(url)
+    tag = tag_entry.get()
+    url_tag = f'https://cataas.com/cat/{tag}' if tag else 'https://cataas.com/cat'
+    img = load_image(url_tag)
     if img:
         new_window = Toplevel()
         new_window.title('Картинка с котиком')
@@ -37,16 +39,20 @@ window = Tk()
 window.title('Cats!')
 window.geometry('600x520')
 
+tag_entry = Entry()
+tag_entry.pack()
+
+load_button = Button(text='Загрузить по тегу', command=open_new_window)
+load_button.pack()
+
 mainmenu = Menu(window)
 window.config(menu=mainmenu)
 filemenu = Menu(mainmenu, tearoff=0)
-filemenu.add_command(label='Загрузить нового котика', command=open_new_window())
+filemenu.add_command(label='Загрузить нового котика', command=open_new_window)
 filemenu.add_separator()
 filemenu.add_command(label='Выход', command=exit)
 mainmenu.add_cascade(label='Click here', menu=filemenu)
 
 url = 'https://cataas.com/cat'
-
-# open_new_window()
 
 window.mainloop()
